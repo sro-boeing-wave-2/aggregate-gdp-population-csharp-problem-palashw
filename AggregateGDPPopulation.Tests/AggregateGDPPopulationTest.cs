8755217b984dc71167bd67536eb3f5cf817e32cd
Palash <palashw173@gmail.com>
2018-07-24 10:29:19 +0530

    Aggregate GDP CSharp done with sync

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,13 +1,36 @@
 using System;
 using Xunit;
+using AggregateGDPPopulation;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace AggregateGDPPopulation.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void test1()
         {
+            Class1.AggregateGdpPop(@"D:\workspace\CSharp\Assignments\aggregate-gdp-population-csharp-problem-palashw\AggregateGDPPopulation\data\datafile.csv");
+            string actual = File.ReadAllText(@"D:\workspace\CSharp\Assignments\aggregate-gdp-population-csharp-problem-palashw\AggregateGDPPopulation.Tests\expected-output.json");
+            string expected = File.ReadAllText(@"D:\workspace\CSharp\Assignments\aggregate-gdp-population-csharp-problem-palashw\AggregateGDPPopulation\data\output.json");
+            Dictionary<string, POPGDPObject> actualjson = JsonConvert.DeserializeObject<Dictionary<string, POPGDPObject>>(actual);
+            Dictionary<string, POPGDPObject> expectedjson = JsonConvert.DeserializeObject<Dictionary<string, POPGDPObject>>(expected);
+            foreach (var key in actualjson.Keys)
+            {
+                if (expectedjson.ContainsKey(key))
+                {
+                    Assert.Equal(actualjson[key].GDP_2012, expectedjson[key].GDP_2012);
+                    Assert.Equal(actualjson[key].POPULATION_2012, expectedjson[key].POPULATION_2012);
+                }
+                else
+                {
+                    Assert.True(false);
+                }
+            }
 
         }
     }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,80 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using Newtonsoft.Json;
 
 namespace AggregateGDPPopulation
 {
+    public class POPGDPObject
+    {
+        public float POPULATION_2012 { get; set; }
+        public float GDP_2012 { get; set; }
+
+    };
+
     public class Class1
     {
+        public static void AggregateGdpPop(string filename)
+        {   //reading in csv data and making headers array
+            string[] csvdata = File.ReadAllLines(filename);
+            string[] headers = csvdata[0].Split(',');
+
+            int indexcountry = Array.IndexOf(headers, "\"Country Name\"");
+            int indexgdp = Array.IndexOf(headers, "\"GDP Billions (USD) 2012\"");
+            int indexpop = Array.IndexOf(headers, "\"Population (Millions) 2012\"");
+
+            string[] countrymap = File.ReadAllLines(@"D:\workspace\CSharp\Assignments\aggregate-gdp-population-csharp-problem-palashw\AggregateGDPPopulation\data\countriesmap.txt");
+            Dictionary<string, string> mapper = new Dictionary<string, string>();
+
+            foreach (string str in countrymap)
+            {
+                string[] row = str.Split(',');
+                mapper[row[0]] = row[1];
+            }
+
+            Dictionary<string, POPGDPObject> finalobjects = new Dictionary<string, POPGDPObject>();
+
+            try
+            {
+
+                for (int i = 1; i < csvdata.Length - 1; i++)
+                {
+                    string[] datarow = csvdata[i].Replace("\"", "").Split(',');
+                    
+                    if (!finalobjects.ContainsKey(mapper[datarow[indexcountry]]))
+                    {
+                        finalobjects[mapper[datarow[indexcountry]]] = new POPGDPObject();
+                        finalobjects[mapper[datarow[indexcountry]]]
+                          .GDP_2012 = float.Parse(datarow[indexgdp]);
+                        finalobjects[mapper[datarow[indexcountry]]]
+                          .POPULATION_2012 = float.Parse(datarow[indexpop]);
+                    }
+                    else
+                    {
+                        finalobjects[mapper[datarow[indexcountry]]]
+                          .GDP_2012 += float.Parse(datarow[indexgdp]);
+                        finalobjects[mapper[datarow[indexcountry]]]
+                          .POPULATION_2012 += float.Parse(datarow[indexpop]);
+                    }
+
+                }
+
+            }
+            catch (Exception){ }
+
+            var jsonString = JsonConvert.SerializeObject(finalobjects);
+            File.WriteAllText(@"D:\workspace\CSharp\Assignments\aggregate-gdp-population-csharp-problem-palashw\AggregateGDPPopulation\data\output.json", jsonString);
+            //string oppath = @"D:\workspace\CSharp\Assignments\aggregate - gdp - population - csharp - problem - palashw\AggregateGDPPopulation\data\output";
+            //try
+            //{
+            //    File.WriteAllText(oppath+@"\output.json", jsonString);
+            //}
+            //catch (Exception e)
+            //{
+            //    Directory.CreateDirectory(oppath);
+            //    File.WriteAllText(oppath + @"\output.json", jsonString);
+            //}
+        }
     }
+
 }
